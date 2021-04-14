0e6fee18feff38fea5f37b929cab4e668d29fcac
KarlEIuff <iuff12345@gmail.com>
2021-04-14 11:51:57 +0200

    Added variables in IndestructibleTile class

--- OOP-project2-group1/IndestructibleTile.cs
+++ OOP-project2-group1/IndestructibleTile.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace MJU20BreakoutClone
 {
-    class IndestructibleTile
+    class IndestructibleTile : Rectangle
 {
+        width = 0.0; //Temporary
+        height = 0.0;
+        xPos = 0.0;
+        yPos = 0.0;
 }
 }
