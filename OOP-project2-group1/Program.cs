370aa25ca7ba3b929b67e1ed592c8a4c526b553d
Attben <72492246+Attben@users.noreply.github.com>
2021-04-14 13:26:02 +0200

    Code skeleton for main update loop.

--- OOP-project2-group1/Program.cs
+++ OOP-project2-group1/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Diagnostics; //Stopwatch
+using System.Threading;
 
-namespace ConsoleApp3
+namespace MJU20BreakoutClone
 {
     class Program
     {
+		const double targetFrameRate = 1.0;
+		const long millisecondsPerSecond = 1000;
+		const long targetFrameDelay = (long)(millisecondsPerSecond / targetFrameRate);
+		
+		static void getGameInputs()
+		{
+			//NYI
+		}
+		
+		static void updateGameState(double deltatime)
+		{
+			//NYI
+		}
+		
+		static void renderGame()
+		{
+			//NYI
+		}
+		
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool running = true;
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			long previousFrameTime;
+			long currentFrameTime = stopwatch.ElapsedMilliseconds;
+			
+			while(running)
+			{
+				previousFrameTime = currentFrameTime;
+				currentFrameTime = stopwatch.ElapsedMilliseconds;
+				long frameDelay = currentFrameTime - previousFrameTime;
+				double deltatime = (double)frameDelay;
+				
+				getGameInputs();
+				updateGameState(deltatime);
+				renderGame();
+				
+				if(frameDelay < targetFrameDelay)
+				{
+					Thread.Sleep((int)(targetFrameDelay - frameDelay));
+				}
+			}
         }
     }
 }
