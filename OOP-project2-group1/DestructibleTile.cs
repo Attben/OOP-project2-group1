017278d2c7b1a1cd1c26c069e0ada965a838fafa
Karl Eriksson <k_e.ericson@hotmail.com>
2021-04-14 10:24:31 +0200

    Added DestructibleTile class

---
+++ OOP-project2-group1/DestructibleTile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MJU20BreakoutClone
+{
+    class DestructibleTile : Rectangle
+{
+        public void destroy()
+        {
+            //ascii explosion
+        }
+}
+}
--- OOP-project2-group1/Program.cs
+++ OOP-project2-group1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ConsoleApp3
+namespace MJU20BreakoutClone
 {
     class Program
     {
