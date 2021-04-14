5192ec16749250e41b87f5df3b9e81cfc19f03a3
Filip Stang <Filip.Stang@outlook.com>
2021-04-14 22:21:50 +0200

    removed old code

--- OOP-project2-group1/Rectangle.cs
+++ OOP-project2-group1/Rectangle.cs
@@ -11,12 +11,6 @@ namespace MJU20BreakoutClone
         public int Width { get; set; }
 
         public int Height { get; set; }
-        #region MyRegion
-
-        //TODO: En array med rektangelns alla egenskaper? används i Collision detection
-        //TODO: EN tvådimentionel array av typen Rectangle som ritas på consolen'
-
-        #endregion
         public Rectangle(int x , int y, int width, int height)
         {
             this.X = x;
@@ -25,22 +19,6 @@ namespace MJU20BreakoutClone
             this.Height = height;
             
         }
-        /*
-        private Rectangle[] MakeRectangle()
-        {
-
-        }
-        public void DrawRectangle(Rectangle[,] rect )
-        {
-            for (int i = 0; i < length; i++)
-            {
-                for (int i = 0; i < length; i++)
-                {
-
-                }
-            }
-        }
-        */
         private bool IsCollisionDetected(Rectangle rect1, Rectangle rect2)
         {
             if (rect1.X < rect2.X + rect2.Width &&
