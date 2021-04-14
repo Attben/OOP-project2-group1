2c2508f7d2f27cdbd90a477129fb15e2ca659deb
Filip Stang <Filip.Stang@outlook.com>
2021-04-14 23:44:03 +0200

    Input skelet

--- OOP-project2-group1/MethodsContainer.cs
+++ OOP-project2-group1/MethodsContainer.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace OOP-project2-group1
+
+namespace MJU20BreakoutClone
 {
-    class MethodsContainer
-{
-}
+    public static class MethodsContainer
+    {
+
+        public static void UserInput()
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.LeftArrow://TODO paddle moves left
+                    break;
+                case ConsoleKey.RightArrow://TODO Paddle Moves Right
+                    break;
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                    Console.Write("Game is closed.");
+                    return;
+                default:
+                    break;
+            }
+            
+        }
+    }
 }
